dfab02a48c1de22ff7130cfda81b02b814f028c9
Alejitoo <a4jzaza@gmail.com>
2025-03-12 21:11:48 -0500

    Angulos Commit

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ b = int(input("Ingrese el valor del lado b: "))
 c = int(input("Ingrese el valor del lado c: "))
 
 main(a, b, c)
+
+Anguloa= math.degrees(math.acos((b**2+c**2-a**2)/(2*b*c)))
+Angulob= math.degrees(math.acos((a**2+c**2-b**2)/(2*a*c)))
+Anguloc= 180-(Anguloa+Angulob)
+
+if Anguloa==90 or Angulob==90 or Anguloc==90:
+    print(f"su triangulo tiene un angulo de 90°\es un triangulo rectanguulo\nAngulo A--> {Anguloa}°\nAngulo B--> {Angulob}°\nAngulo C--> {Anguloc}°")
+elif Anguloa>90 or Angulob>90 or Anguloc>90:
+    print(f"Su triangulo tiene un angulo mayor a 90°\nes un triangulo obtusangulo\Angulo A--> {Anguloa}°\nAngulo B-->{Angulob}°\nAngulo C-->{Anguloc}°")
+else:
+    print(f"Su triangulo no tiene angulos mayores o iguales a 90°\es un angulo acutangulo\nAngulo A-->{Anguloa}°\nAngulo B--> {Angulob}°\nAngulo C-->{Anguloc}°")
