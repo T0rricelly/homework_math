62165b32a64c0ee8cf6386a0f4f460de66d0efb9
santiago <kevintorres1609@gmail.com>
2025-03-12 20:08:45 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+
+def main(lado1, lado2, lado3):
+    if lado1 + lado2 <= lado3 or lado2 + lado3 <= lado1 or lado1 + lado3 <= lado3:
+        print("No es un triangulo valido")
+    else:
+        print("Es un triangulo")
+        
+# Datos 
+a = int(input("Ingrese el valor del lado a: "))
+b = int(input("Ingrese el valor del lado b: "))
+c = int(input("Ingrese el valor del lado c: "))
+
+main(a, b, c)
